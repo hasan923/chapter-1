f91f16e5ca84aef2cd016ae45dec49aea683294a
hasan923 <94882385+hasan923@users.noreply.github.com>
2021-11-23 10:02:36 +0500

    Exercise 04_01

--- chapter01/Exercise04_01.py
+++ chapter01/Exercise04_01.py
@@ -1,6 +1,8 @@
-# Display the table
-print("a    a^2    a^3")
-print("1    1      1")
-print("2    4      8")
-print("3    9      27")
-print("4    16     64")
+a = 1
+
+print("a\ta^2\ta^3")
+while a < 5:
+        b = a ** 2
+        c = a ** 3
+        print(int(a), "\t", int(b), "\t", int(c))
+        a += 1
